ae84cfcd58a3477853c9bdae30836ed0c798f4de
Ketouem <cyril.thomas@surycat.com>
2019-05-24 17:27:01 +0200

    Changes for tf 0.12.0

--- module/lambda.tf
+++ module/lambda.tf
@@ -3,19 +3,24 @@
 #
 
 data "null_data_source" "lambda_archive" {
-  inputs {
+  inputs = {
     filename = "${substr("${path.module}/lambda/sns-to-slack.zip", length(path.cwd) + 1, -1)}"
   }
 }
 
 resource "aws_lambda_function" "sns_to_slack" {
-  filename         = "${data.null_data_source.lambda_archive.outputs.filename}"
+  filename         = "${data.null_data_source.lambda_archive.outputs["filename"]}"
   function_name    = "${var.lambda_function_name}"
   description      = "Sends SNS events to Slack"
   role             = "${aws_iam_role.lambda_sns_to_slack.arn}"
   handler          = "lambda_function.lambda_handler"
-  source_code_hash = "${base64sha256(file("${path.module}/lambda/sns-to-slack.zip"))}"
+  source_code_hash = "${filebase64sha256("${path.module}/lambda/sns-to-slack.zip")}"
   runtime          = "python2.7"
+
+  # TO FIX
+  lifecycle {
+    ignore_changes = [filename, last_modified]
+  }
 
   environment {
     variables = {
